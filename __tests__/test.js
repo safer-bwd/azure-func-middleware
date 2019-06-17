f73a8f97cb7ff4f20a63503f5f0c04135093d355
safer <asafron2@ford.com>
2019-06-17 12:01:30 +0300

    add test: useIf() and useChain()

--- __tests__/test.js
+++ __tests__/test.js
@@ -332,3 +332,159 @@ describe('chain with an unhandled error #2', () => {
     expect(calls[0][0]).toBe('use1');
   });
 });
+
+describe('chain with useIf()', () => {
+  let handler;
+
+  beforeAll(() => {
+    handler = new AzureFuncMiddleware()
+      .use(async (ctx, next) => {
+        ctx.log.info('use1');
+        ctx.state.count = 1;
+        await wait();
+        next();
+      })
+      .useIf(ctx => ctx.state.count === 2, async (ctx, next) => {
+        ctx.log.info('useIf1');
+        ctx.state.count += 1;
+        await wait();
+        next();
+      })
+      .useIf(ctx => ctx.state.count === 1, async (ctx, next) => {
+        ctx.log.info('useIf2');
+        ctx.state.count += 1;
+        await wait();
+        next();
+      })
+      .use(async (ctx) => {
+        ctx.log.info('use2');
+        ctx.state.count += 1;
+        await wait();
+        ctx.done(null, { status: 200, body: ctx.state.count });
+      })
+      .listen();
+  });
+
+  it('should resolve a correct response', async () => {
+    const context = createContext();
+    const { status, body } = await handler(context);
+    expect(status).toEqual(200);
+    expect(body).toEqual(3);
+  });
+
+  it('should be a correct order', async () => {
+    const ctx = createContext();
+    await handler(ctx);
+    const { calls } = ctx.log.info.mock;
+    expect(calls.length).toBe(3);
+    const callsArgs = calls.map(([arg]) => arg);
+    expect(callsArgs).toEqual(['use1', 'useIf2', 'use2']);
+  });
+});
+
+describe('chain with useChain() #1', () => {
+  let handler;
+
+  beforeAll(() => {
+    handler = new AzureFuncMiddleware()
+      .use(async (ctx, next) => {
+        ctx.log.info(1);
+        ctx.state.count = 1;
+        await wait();
+        next();
+      })
+      .useChain([
+        async (ctx, next) => {
+          ctx.log.info(2);
+          ctx.state.count += 1;
+          await wait();
+          next();
+        },
+        async (ctx, next) => {
+          ctx.log.info(3);
+          ctx.state.count += 1;
+          await wait();
+          next();
+        },
+      ])
+      .use(async (ctx) => {
+        ctx.log.info(4);
+        ctx.state.count += 1;
+        await wait();
+        ctx.done(null, { status: 200, body: ctx.state.count });
+      })
+      .listen();
+  });
+
+  it('should resolve a correct response', async () => {
+    const context = createContext();
+    const { status, body } = await handler(context);
+    expect(status).toEqual(200);
+    expect(body).toEqual(4);
+  });
+
+  it('should be a correct order', async () => {
+    const ctx = createContext();
+    await handler(ctx);
+    const { calls } = ctx.log.info.mock;
+    expect(calls.length).toBe(4);
+    const callsArgs = calls.map(([arg]) => arg);
+    expect(callsArgs).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('chain with useChain() #2', () => {
+  let handler;
+
+  beforeAll(() => {
+    handler = new AzureFuncMiddleware()
+      .use(async (ctx, next) => {
+        ctx.log.info(1);
+        ctx.state.count = 1;
+        await wait();
+        next();
+      })
+      .useChain([
+        {
+          fn: async (ctx, next) => {
+            ctx.log.info(2);
+            ctx.state.count += 1;
+            await wait();
+            next();
+          },
+        },
+        {
+          predicate: ctx => ctx.state.count === 1,
+          fn: async (ctx, next) => {
+            ctx.log.info(3);
+            ctx.state.count += 1;
+            await wait();
+            next();
+          },
+        },
+      ])
+      .use(async (ctx) => {
+        ctx.log.info(4);
+        ctx.state.count += 1;
+        await wait();
+        ctx.done(null, { status: 200, body: ctx.state.count });
+      })
+      .listen();
+  });
+
+  it('should resolve a correct response', async () => {
+    const context = createContext();
+    const { status, body } = await handler(context);
+    expect(status).toEqual(200);
+    expect(body).toEqual(3);
+  });
+
+  it('should be a correct order', async () => {
+    const ctx = createContext();
+    await handler(ctx);
+    const { calls } = ctx.log.info.mock;
+    expect(calls.length).toBe(3);
+    const callsArgs = calls.map(([arg]) => arg);
+    expect(callsArgs).toEqual([1, 2, 4]);
+  });
+});
