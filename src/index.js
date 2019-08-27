f5bc7126af6f36fcc1710b3eee47ea340948eb1f
safer <asafron2@ford.com>
2019-08-27 09:31:06 +0300

    refactor&style

--- src/index.js
+++ src/index.js
@@ -37,7 +37,6 @@ class AzureFuncMiddleware {
       const middleware = createMiddleware(props);
       this.middlewares.push(middleware);
     });
-
     return this;
   }
 
@@ -67,6 +66,7 @@ class AzureFuncMiddleware {
           if (!this.silent) log(new Error('done() called multiple times'));
           return;
         }
+
         doneCalled = true;
 
         const [err, result] = args;
@@ -80,9 +80,12 @@ class AzureFuncMiddleware {
       const handle = async (index, error) => {
         const middleware = this.middlewares[index];
         if (!middleware) {
-          if (error && !doneCalled) {
-            donePromise.reject(error);
-          }
+          if (error && !doneCalled) donePromise.reject(error);
+          return;
+        }
+
+        if (!middleware.needExec(error, ctx)) {
+          handle(index + 1, error);
           return;
         }
 
@@ -96,11 +99,6 @@ class AzureFuncMiddleware {
           return handle(index + 1, err);
         };
 
-        if (!middleware.needExec(error, ctx)) {
-          next(error);
-          return;
-        }
-
         try {
           await middleware.exec(error, ctx, next);
         } catch (err) {
@@ -108,7 +106,7 @@ class AzureFuncMiddleware {
             if (!this.silent) log('unhandled error after next() called', err);
             return;
           }
-          next(err);
+          handle(index + 1, err);
         }
       };
 
--- src/middleware/create.js
+++ src/middleware/create.js
@@ -3,7 +3,7 @@ import ErrorMiddleware from './ErrorMiddleware';
 
 export default (props) => {
   const { fn, predicate, isError } = props;
-  
+
   return isError
     ? new ErrorMiddleware(fn)
     : new Middleware(fn, predicate);
