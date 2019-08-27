4da412a585b499ca981eea7e8d07a791c4c975ae
safer <asafron2@ford.com>
2019-08-27 09:12:55 +0300

    fix(utils/logger)

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,5 @@
 export default ctx => (...args) => {
-  if (ctx.log && ctx.log.error) {
+  if (ctx.log && ctx.log.warn) {
     ctx.log.warn(...args);
   } else {
     // eslint-disable-next-line no-console
